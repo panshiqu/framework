f74bf6e3c7a399523ef2a8f4e8392c35c184cd5d
panshiqu <panshiqu@sina.com>
2017-04-20 14:55:37 +0800

    first commit

---
+++ network/server.go
@@ -0,0 +1,95 @@
+package network
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"sync"
+)
+
+// Processor 处理器
+type Processor interface {
+}
+
+// Server 服务器
+type Server struct {
+	listener  net.Listener
+	processor Processor
+
+	mutex       sync.Mutex
+	waitgroup   sync.WaitGroup
+	connections map[net.Conn]bool
+}
+
+// NewServer 创建服务器
+func NewServer(address string, processor Processor) *Server {
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &Server{
+		listener:    listener,
+		processor:   processor,
+		connections: make(map[net.Conn]bool),
+	}
+}
+
+// Start 开始服务
+func (s *Server) Start() error {
+	for {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			s.stop()
+			return err
+		}
+
+		go s.handleConn(conn)
+	}
+}
+
+func (s *Server) stop() {
+	for conn := range s.connections {
+		conn.Close()
+	}
+
+	s.waitgroup.Wait()
+}
+
+// Stop 停止服务
+func (s *Server) Stop() {
+	s.listener.Close()
+}
+
+func (s *Server) addConn(conn net.Conn) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	s.connections[conn] = true
+}
+
+func (s *Server) removeConn(conn net.Conn) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	delete(s.connections, conn)
+}
+
+func (s *Server) handleConn(conn net.Conn) {
+	s.addConn(conn)
+	defer s.removeConn(conn)
+
+	s.waitgroup.Add(1)
+	defer s.waitgroup.Done()
+
+	for {
+		buf := make([]byte, 5)
+		n, err := conn.Read(buf)
+
+		log.Println(n, err, string(buf))
+		if err != nil {
+			break
+		}
+
+		fmt.Fprintf(conn, "hello")
+	}
+}
