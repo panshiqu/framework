36fced97561d8b8be2a3d987aaff17c26c6b5ffc
panshiqu <panshiqu@sina.com>
2017-05-27 17:42:22 +0800

    finish register service

--- manager/processor.go
+++ manager/processor.go
@@ -1,6 +1,7 @@
 package manager
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net"
@@ -24,7 +25,7 @@ func (p *Processor) OnMessage(conn net.Conn, mcmd uint16, scmd uint16, data []by
 		return p.OnMainCommon(conn, scmd, data)
 	}
 
-	return define.NewError(fmt.Sprint("unknown main cmd", mcmd))
+	return define.NewError(fmt.Sprint("unknown main cmd ", mcmd))
 }
 
 // OnMainCommon 通用主命令
@@ -34,12 +35,23 @@ func (p *Processor) OnMainCommon(conn net.Conn, scmd uint16, data []byte) error 
 		return p.OnSubRegisterService(conn, data)
 	}
 
-	return define.NewError(fmt.Sprint("unknown sub cmd", scmd))
+	return define.NewError(fmt.Sprint("unknown sub cmd ", scmd))
 }
 
 // OnSubRegisterService 注册服务子命令
 func (p *Processor) OnSubRegisterService(conn net.Conn, data []byte) error {
-	log.Println(string(data))
+	var service define.Service
+
+	if err := json.Unmarshal(data, &service); err != nil {
+		return define.NewError(err.Error())
+	}
+
+	if _, ok := p.services[service.ID]; ok {
+		return define.NewError("repeat register service")
+	}
+
+	p.services[service.ID] = &service
+
 	return nil
 }
 
@@ -61,6 +73,7 @@ func (p *Processor) OnClientConnect(conn net.Conn) {
 // NewProcessor 创建处理器
 func NewProcessor(server *network.Server) *Processor {
 	return &Processor{
-		server: server,
+		server:   server,
+		services: make(map[int]*define.Service),
 	}
 }
