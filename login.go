fe245b1035d3c6750b00cb32ff04b975ab09cb71
panshiqu <panshiqu@sina.com>
2017-06-12 23:12:57 +0800

    add login.json and read unmarshal it

--- login.go
+++ login.go
@@ -5,8 +5,10 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/panshiqu/framework/define"
 	"github.com/panshiqu/framework/login"
 	"github.com/panshiqu/framework/network"
+	"github.com/panshiqu/framework/utils"
 )
 
 func handleSignal(server *network.Server, client *network.Client) {
@@ -21,6 +23,12 @@ func handleSignal(server *network.Server, client *network.Client) {
 }
 
 func main() {
+	config := &define.ConfigLogin{}
+	if err := utils.ReadJSON("./config/login.json", config); err != nil {
+		log.Println("ReadJSON ConfigLogin", err)
+		return
+	}
+
 	server := network.NewServer("127.0.0.1:8081")
 	client := network.NewClient("127.0.0.1:8080")
 	processor := login.NewProcessor(server, client)
