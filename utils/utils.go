99d5b9ecbc65e27e05c187a5530904cc73b40138
panshiqu <panshiqu@sina.com>
2017-06-12 23:02:19 +0800

    utils add ReadJSON function

---
+++ utils/utils.go
@@ -0,0 +1,28 @@
+package utils
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+)
+
+// ReadJSON 打开读取解析JSON文件
+func ReadJSON(name string, js interface{}) error {
+	f, err := os.Open(name)
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	body, err := ioutil.ReadAll(f)
+	if err != nil {
+		return err
+	}
+
+	if err := json.Unmarshal(body, js); err != nil {
+		return err
+	}
+
+	return nil
+}
