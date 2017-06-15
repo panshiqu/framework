b1204b00cd20c95b72e55eea230a7b655cde5775
panshiqu <panshiqu@sina.com>
2017-06-15 15:09:12 +0800

    add Error struct and CheckError interface

--- define/error.go
+++ define/error.go
@@ -1,10 +1,38 @@
 package define
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 )
 
+const (
+	// ErrSuccess 成功
+	ErrSuccess int = 0
+)
+
+// Error 错误
+type Error struct {
+	Errno   int    `json:",omitempty"` // 错误码
+	Errdesc string `json:",omitempty"` // 错误描述
+}
+
 // NewError 创建错误
 func NewError(desc string) error {
-	return fmt.Errorf(`{"errno":1,"errdesc":"%s"}`, desc)
+	return fmt.Errorf(`{"Errno":1,"Errdesc":"%s"}`, desc)
+}
+
+// CheckError 检查错误
+func CheckError(data []byte) error {
+	eno := &Error{}
+
+	if err := json.Unmarshal(data, eno); err != nil {
+		return err
+	}
+
+	if eno.Errno != ErrSuccess {
+		return errors.New(string(data))
+	}
+
+	return nil
 }
