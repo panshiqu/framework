e69c165498c6514cf39468f1fb6e19d342dcbf01
panshiqu <panshiqu@sina.com>
2017-06-23 18:26:56 +0800

    utils add Signature

--- utils/utils.go
+++ utils/utils.go
@@ -1,9 +1,14 @@
 package utils
 
 import (
+	"crypto/sha1"
 	"encoding/json"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
+	"sort"
+	"strings"
 	"time"
 )
 
@@ -11,6 +16,16 @@ import (
 func Date() int {
 	y, m, d := time.Now().Date()
 	return y*10000 + int(m)*100 + d
+}
+
+// Signature 签名
+func Signature(timestamp int64) string {
+	s := fmt.Sprintf("%s%d", "116227", timestamp)
+	ss := strings.Split(s, "")
+	sort.Strings(ss)
+	sha := sha1.New()
+	io.WriteString(sha, strings.Join(ss, ""))
+	return fmt.Sprintf("%x", sha.Sum(nil))
 }
 
 // ReadJSON 打开读取解析JSON文件
