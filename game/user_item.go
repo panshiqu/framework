ac9d41fadcb531ef329b477a874b84e0296c1e77
panshiqu <panshiqu@sina.com>
2017-07-22 15:51:57 +0800

    usermanager add create and remove

--- game/user_item.go
+++ game/user_item.go
@@ -1,15 +1,18 @@
 package game
 
+import "net"
+
 // UserItem 用户
 type UserItem struct {
-	id      int    // 编号
-	name    string // 名称
-	icon    int    // 图标
-	level   int    // 等级
-	gender  int    // 性别
-	score   int64  // 分数
-	diamond int64  // 钻石
-	robot   bool   // 机器人
+	id      int      // 编号
+	name    string   // 名称
+	icon    int      // 图标
+	level   int      // 等级
+	gender  int      // 性别
+	score   int64    // 分数
+	diamond int64    // 钻石
+	robot   bool     // 机器人
+	conn    net.Conn // 网络连接
 }
 
 // UserID 用户编号
--- game/user_manager.go
+++ game/user_manager.go
@@ -1,5 +1,42 @@
 package game
 
+import (
+	"net"
+	"sync"
+
+	"github.com/panshiqu/framework/define"
+)
+
 // UserManager 用户管理
 type UserManager struct {
+	mutex sync.Mutex
+	users map[int]*UserItem
+}
+
+// Remove 移除用户
+func (u *UserManager) Remove(id int) {
+	u.mutex.Lock()
+	delete(u.users, id)
+	u.mutex.Unlock()
+}
+
+// Create 创造用户
+func (u *UserManager) Create(conn net.Conn, reply *define.ReplyFastLogin) *UserItem {
+	userItem := &UserItem{
+		id:      reply.UserID,
+		name:    reply.UserName,
+		icon:    reply.UserIcon,
+		level:   reply.UserLevel,
+		gender:  reply.UserGender,
+		score:   reply.UserScore,
+		diamond: reply.UserDiamond,
+		robot:   reply.IsRobot,
+		conn:    conn,
+	}
+
+	u.mutex.Lock()
+	u.users[userItem.UserID()] = userItem
+	u.mutex.Unlock()
+
+	return userItem
 }
