7c6a83b373b355182f5280b9884c664c7bd59e83
panshiqu <panshiqu@sina.com>
2017-08-28 17:57:00 +0800

    just for race detection

--- game/table_frame.go
+++ game/table_frame.go
@@ -2,6 +2,8 @@ package game
 
 import (
 	"encoding/json"
+	"sync"
+	"sync/atomic"
 
 	"github.com/panshiqu/framework/define"
 )
@@ -9,7 +11,8 @@ import (
 // TableFrame 桌子框架
 type TableFrame struct {
 	id     int         // 编号
-	status int         // 状态
+	status int32       // 状态
+	mutex  sync.Mutex  // 加锁
 	users  []*UserItem // 用户
 }
 
@@ -19,14 +22,21 @@ func (t *TableFrame) TableID() int {
 }
 
 // TableStatus 桌子状态
-func (t *TableFrame) TableStatus() int {
-	return t.status
+func (t *TableFrame) TableStatus() int32 {
+	return atomic.LoadInt32(&t.status)
+}
+
+// TableUser 桌子用户
+func (t *TableFrame) TableUser(chair int) *UserItem {
+	t.mutex.Lock()
+	defer t.mutex.Unlock()
+	return t.users[chair]
 }
 
 // UserCount 用户数量
 func (t *TableFrame) UserCount() (cnt int) {
-	for _, v := range t.users {
-		if v != nil {
+	for i := 0; i < define.CG.UserPerTable; i++ {
+		if t.TableUser(i) != nil {
 			cnt++
 		}
 	}
@@ -36,8 +46,8 @@ func (t *TableFrame) UserCount() (cnt int) {
 
 // ReadyCount 准备数量
 func (t *TableFrame) ReadyCount() (cnt int) {
-	for _, v := range t.users {
-		if v != nil && v.UserStatus() == define.UserStatusReady {
+	for i := 0; i < define.CG.UserPerTable; i++ {
+		if user := t.TableUser(i); user != nil && user.UserStatus() == define.UserStatusReady {
 			cnt++
 		}
 	}
@@ -45,57 +55,57 @@ func (t *TableFrame) ReadyCount() (cnt int) {
 	return
 }
 
-// NilChairID 空椅子编号
-func (t *TableFrame) NilChairID() int {
-	for k, v := range t.users {
-		if v == nil {
-			return k
-		}
-	}
-
-	return define.InvalidChair
-}
-
 // SetUserStatus 设置用户状态
 func (t *TableFrame) SetUserStatus(status int) {
-	for _, v := range t.users {
-		if v != nil {
-			v.SetUserStatus(status)
+	for i := 0; i < define.CG.UserPerTable; i++ {
+		if user := t.TableUser(i); user != nil {
+			user.SetUserStatus(status)
 		}
 	}
 }
 
 // SitDown 坐下
 func (t *TableFrame) SitDown(userItem *UserItem) {
-	chair := t.NilChairID()
+	t.mutex.Lock()
+	chair := define.InvalidChair
+	for k, v := range t.users {
+		if v == nil {
+			chair = k
+			break
+		}
+	}
 	t.users[chair] = userItem
 	userItem.SetChairID(chair)
 	userItem.SetTableFrame(t)
+	t.mutex.Unlock()
 
 	// 广播我的坐下
 	t.SendTableJSONMessage(define.GameCommon, define.GameNotifySitDown, userItem.TableUserInfo())
 
-	for _, v := range t.users {
-		if v == nil || v == userItem {
+	for i := 0; i < define.CG.UserPerTable; i++ {
+		if i == chair {
 			continue
 		}
 
-		// 已有用户坐下
-		userItem.SendJSONMessage(define.GameCommon, define.GameNotifySitDown, v.TableUserInfo())
+		if user := t.TableUser(i); user != nil {
+			userItem.SendJSONMessage(define.GameCommon, define.GameNotifySitDown, user.TableUserInfo())
+		}
 	}
 
 	// 正在游戏设置用户状态
-	if t.status == define.TableStatusGame {
+	if t.TableStatus() == define.TableStatusGame {
 		userItem.SetUserStatus(define.UserStatusPlaying)
 	}
 }
 
 // StandUp 站起
 func (t *TableFrame) StandUp(userItem *UserItem) {
+	t.mutex.Lock()
 	chair := userItem.ChairID()
 	t.users[chair] = nil
 	userItem.SetChairID(define.InvalidChair)
 	userItem.SetTableFrame(nil)
+	t.mutex.Unlock()
 
 	standUp := &define.NotifyStandUp{
 		ChairID: chair,
@@ -107,35 +117,29 @@ func (t *TableFrame) StandUp(userItem *UserItem) {
 
 // StartGame 开始游戏
 func (t *TableFrame) StartGame() {
-	// 校验桌子状态
-	if t.status == define.TableStatusGame {
-		return
-	}
-
 	// 检查准备数量
 	if t.ReadyCount() < define.CG.MinReadyStart {
 		return
 	}
 
+	// 设置桌子状态
+	if !atomic.CompareAndSwapInt32(&t.status, define.TableStatusFree, define.TableStatusGame) {
+		return
+	}
+
 	// 设置用户状态
 	t.SetUserStatus(define.UserStatusPlaying)
-
-	// 设置桌子状态
-	t.status = define.TableStatusGame
 }
 
 // ConcludeGame 结束游戏
 func (t *TableFrame) ConcludeGame() {
-	// 校验桌子状态
-	if t.status == define.TableStatusFree {
+	// 设置桌子状态
+	if !atomic.CompareAndSwapInt32(&t.status, define.TableStatusGame, define.TableStatusFree) {
 		return
 	}
 
 	// 设置用户状态
 	t.SetUserStatus(define.UserStatusFree)
-
-	// 设置桌子状态
-	t.status = define.TableStatusFree
 }
 
 // OnTimer 定时器
@@ -150,8 +154,8 @@ func (t *TableFrame) OnMessage(mcmd uint16, scmd uint16, data []byte) {
 
 // SendTableMessage 发送桌子消息
 func (t *TableFrame) SendTableMessage(mcmd uint16, scmd uint16, data []byte) {
-	for k := range t.users {
-		t.SendChairMessage(k, mcmd, scmd, data)
+	for i := 0; i < define.CG.UserPerTable; i++ {
+		t.SendChairMessage(i, mcmd, scmd, data)
 	}
 }
 
@@ -164,8 +168,8 @@ func (t *TableFrame) SendTableJSONMessage(mcmd uint16, scmd uint16, js interface
 
 // SendChairMessage 发送椅子消息
 func (t *TableFrame) SendChairMessage(chair int, mcmd uint16, scmd uint16, data []byte) {
-	if userItem := t.users[chair]; userItem != nil {
-		userItem.SendMessage(mcmd, scmd, data)
+	if user := t.TableUser(chair); user != nil {
+		user.SendMessage(mcmd, scmd, data)
 	}
 }
 
