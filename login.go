0658ffe4d8af9dfd40e66164b9829e56d7c09bed
farmercode <2494053017@qq.com>
2019-03-01 16:59:51 +0800

    修改相关导入库从github到本地

--- login.go
+++ login.go
@@ -5,12 +5,13 @@ import (
 	"os"
 	"os/signal"
 
-	"github.com/panshiqu/framework/define"
-	"github.com/panshiqu/framework/login"
-	"github.com/panshiqu/framework/network"
-	"github.com/panshiqu/framework/utils"
+	"./define"
+	"./login"
+	"./network"
+	"./utils"
 )
 
+//信号处理函数
 func handleSignal(server *network.Server, client *network.Client) {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
@@ -23,6 +24,7 @@ func handleSignal(server *network.Server, client *network.Client) {
 }
 
 func main() {
+	//读取login配置文件
 	config := &define.ConfigLogin{}
 	if err := utils.ReadJSON("./config/login.json", config); err != nil {
 		log.Println("ReadJSON ConfigLogin", err)
