6067cb435a6de5164f912d7f45f8cb1141b851f3
panshiqu <panshiqu@sina.com>
2023-11-30 12:16:12 +0800

    stack

--- utils/stack.go
+++ utils/stack.go
@@ -1,8 +1,11 @@
 package utils
 
 import (
+	"errors"
 	"fmt"
+	"log"
 	"os"
+	"runtime"
 	"runtime/debug"
 	"time"
 )
@@ -11,11 +14,42 @@ import (
 func SafeCall(fn func(...any), args ...any) {
 	defer func() {
 		if err := recover(); err != nil {
-			f, _ := os.OpenFile(time.Now().Format("stack2006-01-02T15:04:05.log"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-			fmt.Fprintln(f, "err:", err)
-			f.Write(debug.Stack())
-			f.Close()
+			errStack(debug.Stack(), err)
 		}
 	}()
 	fn(args...)
+}
+
+func errStack(stack []byte, err ...any) {
+	data := stack
+	if len(err) > 0 {
+		data = []byte(fmt.Sprintf("err: %v\n%s", err[0], string(stack)))
+	}
+	if f, e := os.OpenFile(time.Now().Format("stack20060102150405.log"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644); e != nil {
+		log.Printf("OpenFile %v\n%s", e, string(data))
+	} else {
+		f.Write(data)
+		f.Close()
+	}
+}
+
+func Stack(err any, e ...*error) {
+	if err != nil {
+		if len(e) > 0 {
+			*(e[0]) = errors.New("panic")
+		}
+		errStack(debug.Stack(), err)
+	}
+}
+
+func StackAll() {
+	buf := make([]byte, 1024)
+	for {
+		n := runtime.Stack(buf, true)
+		if n < len(buf) {
+			errStack(buf[:n])
+			return
+		}
+		buf = make([]byte, 2*len(buf))
+	}
 }
